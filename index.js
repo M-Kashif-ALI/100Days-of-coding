51ee340a7e8faad7640511b8a400937cbdf2b70f
M-Kashif-ALI <158289645+M-Kashif-ALI@users.noreply.github.com>
2024-05-27 22:12:11 +0500

    day 1 to 4
    completing quest 1 to day 4

---
+++ index.js
@@ -0,0 +1,42 @@
+"use strict";
+//DAY 1:
+Object.defineProperty(exports, "__esModule", { value: true });
+// Q1
+var Name = "Kashif Ali";
+// console.log(`Hello ${Name}, would you like to learn some TypeScript today?`);
+// Q2
+var personName = 'Ali';
+// console.log(personName.toLowerCase());
+// console.log(personName.toUpperCase());
+// console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+// DAY 2:
+// Q1 & 2
+var person = "Albert Einstein";
+var quote = "A person who never made a mistake never tried anything new.";
+// console.log(`${person} once said, ${quote}`);
+// Q3
+var aName = "\t\n Kashif Ali \t\n";
+// console.log(aName);
+// console.log(aName.trim());
+// DAY 3:
+// Q1 & 2
+// console.log(5 + 3);
+// console.log(10 - 2);
+// console.log(4 * 2);
+// console.log(16 / 2);
+// Q3
+var myFavouriteNum = 6;
+// console.log(`My favourite number is ${myFavouriteNum}`);
+// DAY 4:
+// Q2 
+// let names = ['Arham', 'Zain', 'Mudassir'];
+// for(let i = 0; i < names.length; i++){
+//   console.log(`Hi ${names[i]}, have a nice day`);
+// };
+// Q3
+var names = ['Arham', 'Zain', 'Mudassir'];
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var name_1 = names_1[_i];
+    console.log("Hi ".concat(name_1, ", have a nice day"));
+}
+;
---
+++ index.ts
@@ -0,0 +1,50 @@
+//DAY 1:
+
+// Q1
+let Name = "Kashif Ali";
+// console.log(`Hello ${Name}, would you like to learn some TypeScript today?`);
+
+// Q2
+let personName = 'Ali';
+// console.log(personName.toLowerCase());
+// console.log(personName.toUpperCase());
+// console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+
+// DAY 2:
+
+// Q1 & 2
+let person = "Albert Einstein";
+let quote = "A person who never made a mistake never tried anything new."
+// console.log(`${person} once said, ${quote}`);
+
+// Q3
+let aName = "\t\n Kashif Ali \t\n";
+// console.log(aName);
+// console.log(aName.trim());
+
+// DAY 3:
+
+// Q1 & 2
+// console.log(5 + 3);
+// console.log(10 - 2);
+// console.log(4 * 2);
+// console.log(16 / 2);
+
+// Q3
+let myFavouriteNum = 6;
+// console.log(`My favourite number is ${myFavouriteNum}`);
+
+// DAY 4:
+
+// Q2 
+
+// let names = ['Arham', 'Zain', 'Mudassir'];
+// for(let i = 0; i < names.length; i++){
+//   console.log(`Hi ${names[i]}, have a nice day`);
+// };
+
+// Q3
+let names = ['Arham', 'Zain', 'Mudassir'];
+for(let name of names){
+  console.log(`Hi ${name}, have a nice day`);
+};
